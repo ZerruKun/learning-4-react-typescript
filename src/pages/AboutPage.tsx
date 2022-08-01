7975a8656149d5dec2e6f164d4ec587384ad1453
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-08-01 18:41:08 +0300

    Добавление навигации.

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis eveniet, labore, quis quidem reprehenderit repellendus voluptatem voluptate impedit et ab placeat sapiente quam maiores iste assumenda. Possimus delectus voluptatem rem quae doloremque illum id, aut necessitatibus consequatur, recusandae officia commodi explicabo nisi debitis autem omnis repellat laudantium repellendus unde expedita!
+    </div>
+  )
+}
+
+export default AboutPage
